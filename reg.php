d070fcc3ab20f846eb3f454a82070528ef3d2c04
Sarnovskiy Daniil <99619436+BakerPizza@users.noreply.github.com>
2022-02-17 15:33:12 +0300

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,67 @@
+
+
+<html>
+<head>
+<meta charset="utf-8">
+    <title>Главная</title>  
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="shapka1">
+  <center> <table height="auto" class="shapka3"><tr><td>
+
+  <img src="gruming1.svg" width="150" height="150">
+</td><td>
+<h1 class="text1">  Груминг-студия "Веселый пёс"<br> ИС-18 Сарновский Даниил</h1>
+
+</td></tr></table></center>
+    </div class="shapka1">
+    <table class="content1"><tr><td>
+    </table>
+<br>
+
+<center>
+  <div class="inf1" >  
+  <div class="container-sm">
+    <h2 class="text mb-5">Регистрация</h2>
+    <form>
+    <br>
+    <br>
+    <br>
+    <br>
+        <div class="container-reg">
+            <div class="input-group mb-4">
+                <input placeholder='Введите логин' type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="loginReg">
+            </div>
+            <div class="input-group mb-4">
+                <input placeholder='Введите пароль не короче 8 символов' type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="passwordReg">
+            </div>
+            <div class="input-group mb-4">
+                <input placeholder='Повторите пароль' type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="confirmPassword">
+            </div>
+            <div class="input-group mb-4">
+                <input placeholder='Введите e-mail' type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="email">
+            </div>
+            <div class="input-group mb-4">
+                <input placeholder='Введите имя' type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="name">
+            </div>
+            <div class="input-group mb-4">
+                <input placeholder='Введите фамилию' type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="lastname">
+            </div>
+            <div class="input-group mb-4">
+                <input placeholder='Введите отчество' type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="patronymic">
+            </div>
+            <div class="row mb-3"><div class="msg-reg" id = "msgReg"></div></div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+       <button class="btn btn-primary" id="buttonRegUser">Создать аккаунт</button>
+    </form>
+</div>
+
+
+
+</body>
+</html>
